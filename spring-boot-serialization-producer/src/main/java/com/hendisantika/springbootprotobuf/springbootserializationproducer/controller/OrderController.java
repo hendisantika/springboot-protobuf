8f6b2371df756e484412fada827075f657c9df5d
hendisantika <hendisantika@yahoo.co.id>
2019-03-31 06:08:50 +0700

    #15 - Add getOrdersProto method in Order Controller in spring-boot-serialization-producer module

--- spring-boot-serialization-producer/src/main/java/com/hendisantika/springbootprotobuf/springbootserializationproducer/controller/OrderController.java
+++ spring-boot-serialization-producer/src/main/java/com/hendisantika/springbootprotobuf/springbootserializationproducer/controller/OrderController.java
@@ -3,6 +3,10 @@ package com.hendisantika.springbootprotobuf.springbootserializationproducer.cont
 import com.hendisantika.springbootprotobuf.springbootserializationproducer.service.OrderService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.StopWatch;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -19,4 +23,16 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class OrderController {
     private final OrderService orderService;
+
+    @GetMapping(value = "/proto/order/{totalElement}")
+    public ResponseEntity<OrdersProto.Orders> getOrdersProto(@PathVariable int totalElement) {
+        final StopWatch stopWatch = new StopWatch();
+        stopWatch.start("getOrdersProto");
+
+        final OrdersProto.Orders protobufOrders = orderService.getProtobufOrders(totalElement);
+
+        stopWatch.stop();
+        log.info("Total time in milliseconds getOrdersProto: {}", stopWatch.getLastTaskTimeMillis());
+        return ResponseEntity.ok(protobufOrders);
+    }
 }
