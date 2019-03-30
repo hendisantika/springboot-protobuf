bb55948448ee086048d8884cd27e6e1efb9a1a78
hendisantika <hendisantika@yahoo.co.id>
2019-03-31 06:08:02 +0700

    #13 - Add Order Service in spring-boot-serialization-producer module

---
+++ spring-boot-serialization-producer/src/main/java/com/hendisantika/springbootprotobuf/springbootserializationproducer/service/OrderService.java
@@ -0,0 +1,32 @@
+package com.hendisantika.springbootprotobuf.springbootserializationproducer.service;
+
+import com.hendisantika.springbootprotobuf.springbootserializationproducer.domain.Order;
+import com.hendisantika.springbootprotobuf.springbootserializationproducer.gateway.OrderGateway;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-protobuf
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-03-31
+ * Time: 06:07
+ */
+@Service
+@RequiredArgsConstructor
+public class OrderService {
+
+    private final OrderGateway orderGateway;
+
+    public Collection<Order> getOrders(int totalElements) {
+        return orderGateway.regularOrders(totalElements);
+    }
+
+    public OrdersProto.Orders getProtobufOrders(int totalElements) {
+        return orderGateway.protobufOrders(totalElements);
+    }
+}
