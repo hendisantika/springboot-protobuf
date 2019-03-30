fa4dedcaeb4883958323f418a27ffd3db75892aa
hendisantika <hendisantika@yahoo.co.id>
2019-03-31 05:55:26 +0700

    #3 - Add spring-boot-serialization-producer module

---
+++ spring-boot-serialization-producer/src/main/java/com/hendisantika/springbootprotobuf/springbootserializationproducer/SpringBootSerializationProducerApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootprotobuf.springbootserializationproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSerializationProducerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSerializationProducerApplication.class, args);
+    }
+
+}
---
+++ spring-boot-serialization-producer/src/test/java/com/hendisantika/springbootprotobuf/springbootserializationproducer/SpringBootSerializationProducerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hendisantika.springbootprotobuf.springbootserializationproducer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootSerializationProducerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
