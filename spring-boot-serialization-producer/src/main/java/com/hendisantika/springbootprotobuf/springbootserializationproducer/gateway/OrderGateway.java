45394621c4dee94cdb867df2d9df7bff0b6894cd
hendisantika <hendisantika@yahoo.co.id>
2019-03-31 06:06:35 +0700

    #12 - Add Order Gateway in spring-boot-serialization-producer module

---
+++ spring-boot-serialization-producer/src/main/java/com/hendisantika/springbootprotobuf/springbootserializationproducer/gateway/OrderGateway.java
@@ -0,0 +1,21 @@
+package com.hendisantika.springbootprotobuf.springbootserializationproducer.gateway;
+
+import com.hendisantika.springbootprotobuf.springbootserializationproducer.domain.Order;
+
+import java.util.Collection;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-protobuf
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-03-31
+ * Time: 06:02
+ */
+public interface OrderGateway {
+
+    Collection<Order> regularOrders(int totalElements);
+
+    OrdersProto.Orders protobufOrders(int totalElements);
+}
