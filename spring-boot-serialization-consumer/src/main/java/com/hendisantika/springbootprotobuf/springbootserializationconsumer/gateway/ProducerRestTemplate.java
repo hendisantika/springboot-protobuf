ea2a59ebac98965ec68277ea5a166cd7b71c6de2
hendisantika <hendisantika@yahoo.co.id>
2019-04-01 06:14:51 +0700

    #45 - Add getOrders in ProducerRestTemplate in spring-boot-serialization-consumer module

--- spring-boot-serialization-consumer/src/main/java/com/hendisantika/springbootprotobuf/springbootserializationconsumer/gateway/ProducerRestTemplate.java
+++ spring-boot-serialization-consumer/src/main/java/com/hendisantika/springbootprotobuf/springbootserializationconsumer/gateway/ProducerRestTemplate.java
@@ -1,9 +1,11 @@
 package com.hendisantika.springbootprotobuf.springbootserializationconsumer.gateway;
 
 import com.hendisantika.springbootprotobuf.springbootserializationconsumer.config.ProducerPropertiesConfiguration;
+import com.hendisantika.springbootprotobuf.springbootserializationconsumer.domain.Order;
 import com.hendisantika.springbootprotobuf.springbootserializationconsumer.proto.OrdersProto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
@@ -11,6 +13,7 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
+import java.util.Collection;
 
 /**
  * Created by IntelliJ IDEA.
@@ -48,6 +51,22 @@ public class ProducerRestTemplate implements ProducerGateway {
                 .build();
 
         final ResponseEntity<OrdersProto.Orders> responseEntity = protoRestTemplate.exchange(requestEntity, OrdersProto.Orders.class);
+        return responseEntity.getBody();
+    }
+
+    @Override
+    public Collection<Order> getOrders(int totalElements) {
+        final URI uri = UriComponentsBuilder.fromHttpUrl(producerPropertiesConfiguration.getProducerJsonUrl())
+                .path(String.valueOf(totalElements))
+                .build().encode().toUri();
+
+        final RequestEntity<Void> requestEntity = RequestEntity
+                .get(uri)
+                .build();
+
+        final ResponseEntity<Collection<Order>> responseEntity =
+                defaultRestTemplate.exchange(requestEntity, new ParameterizedTypeReference<Collection<Order>>() {
+                });
         return responseEntity.getBody();
     }
 
