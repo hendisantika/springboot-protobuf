97d4574bfec61fbd3a0ac05885408f7b2a623d78
hendisantika <hendisantika@yahoo.co.id>
2019-04-01 06:13:55 +0700

    #43 - Modify ProducerRestTemplate in spring-boot-serialization-consumer module

--- spring-boot-serialization-consumer/src/main/java/com/hendisantika/springbootprotobuf/springbootserializationconsumer/gateway/ProducerRestTemplate.java
+++ spring-boot-serialization-consumer/src/main/java/com/hendisantika/springbootprotobuf/springbootserializationconsumer/gateway/ProducerRestTemplate.java
@@ -1,6 +1,8 @@
 package com.hendisantika.springbootprotobuf.springbootserializationconsumer.gateway;
 
 import com.hendisantika.springbootprotobuf.springbootserializationconsumer.config.ProducerPropertiesConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
@@ -18,5 +20,14 @@ public class ProducerRestTemplate implements ProducerGateway {
     private final ProducerPropertiesConfiguration producerPropertiesConfiguration;
     private final RestTemplate protoRestTemplate;
     private final RestTemplate defaultRestTemplate;
+
+    @Autowired
+    public ProducerRestTemplate(ProducerPropertiesConfiguration producerPropertiesConfiguration,
+                                @Qualifier("protoRestTemplate") RestTemplate protoRestTemplate,
+                                @Qualifier("defaultRestTemplate") RestTemplate defaultRestTemplate) {
+        this.producerPropertiesConfiguration = producerPropertiesConfiguration;
+        this.protoRestTemplate = protoRestTemplate;
+        this.defaultRestTemplate = defaultRestTemplate;
+    }
 
 }
