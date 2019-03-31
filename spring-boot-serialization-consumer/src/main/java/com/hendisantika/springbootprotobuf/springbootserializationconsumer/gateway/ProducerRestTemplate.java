c3c7e05ab4dde766380c2f2f3e3e449a8581ae39
hendisantika <hendisantika@yahoo.co.id>
2019-04-01 06:14:20 +0700

    #44 - Add getOrdersProto in ProducerRestTemplate in spring-boot-serialization-consumer module

--- spring-boot-serialization-consumer/src/main/java/com/hendisantika/springbootprotobuf/springbootserializationconsumer/gateway/ProducerRestTemplate.java
+++ spring-boot-serialization-consumer/src/main/java/com/hendisantika/springbootprotobuf/springbootserializationconsumer/gateway/ProducerRestTemplate.java
@@ -1,10 +1,16 @@
 package com.hendisantika.springbootprotobuf.springbootserializationconsumer.gateway;
 
 import com.hendisantika.springbootprotobuf.springbootserializationconsumer.config.ProducerPropertiesConfiguration;
+import com.hendisantika.springbootprotobuf.springbootserializationconsumer.proto.OrdersProto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
 
 /**
  * Created by IntelliJ IDEA.
@@ -28,6 +34,21 @@ public class ProducerRestTemplate implements ProducerGateway {
         this.producerPropertiesConfiguration = producerPropertiesConfiguration;
         this.protoRestTemplate = protoRestTemplate;
         this.defaultRestTemplate = defaultRestTemplate;
+    }
+
+    @Override
+    public OrdersProto.Orders getOrdersProto(int totalElements) {
+
+        final URI uri = UriComponentsBuilder.fromHttpUrl(producerPropertiesConfiguration.getProducerProtobufUrl())
+                .path(String.valueOf(totalElements))
+                .build().encode().toUri();
+
+        final RequestEntity<Void> requestEntity = RequestEntity
+                .get(uri)
+                .build();
+
+        final ResponseEntity<OrdersProto.Orders> responseEntity = protoRestTemplate.exchange(requestEntity, OrdersProto.Orders.class);
+        return responseEntity.getBody();
     }
 
 }
