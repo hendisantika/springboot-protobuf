0d2e503e4c3b5f447868ba551787e89b0e6a0009
hendisantika <hendisantika@yahoo.co.id>
2019-04-01 05:46:21 +0700

    #22 - Add spring-boot-serialization-consumer module

---
+++ spring-boot-serialization-consumer/src/main/java/com/hendisantika/springbootprotobuf/springbootserializationconsumer/SpringBootSerializationConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootprotobuf.springbootserializationconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSerializationConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSerializationConsumerApplication.class, args);
+    }
+
+}
---
+++ spring-boot-serialization-consumer/src/test/java/com/hendisantika/springbootprotobuf/springbootserializationconsumer/SpringBootSerializationConsumerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hendisantika.springbootprotobuf.springbootserializationconsumer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootSerializationConsumerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
